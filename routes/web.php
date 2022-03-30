ffad12dca236cbbd45fa7c82a298b2d63987b905
OverThink3r <84136925+OverThink3r@users.noreply.github.com>
2022-03-29 21:37:34 -0600

    V. Refactorizacion de código

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,11 @@ use App\Http\Controllers\RolController;
 use App\Http\Controllers\UsuarioController;
 
 
-Route::get('/aravel', function () {
-    return view('welcome');
-});
-
 Route::get('/test', function () {
     return view('home');
 });
 
-// Route::get('/', function () {
-//     return view('Objetivos.login');
-// });
+
 Route::get('/', function () {
     return view('auth.login');
 });
@@ -29,33 +23,27 @@ Route::get('/menu', function () {
 })->name('menu');
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::post('/agregarUsuario/{id}', [App\Http\Controllers\CarreraController::class, 'agregar_usuario'])->name('agregarUser');
-Route::delete('/eliminarAtributo/{id}', [App\Http\Controllers\CarreraController::class, 'eliminarAtributo'])->name('eliminarAtributo');
-Route::delete('/eliminarObjetivo/{id}', [App\Http\Controllers\CarreraController::class, 'eliminarObjetivo'])->name('eliminarObjetivo');
 
 Route::group(['middleware' => ['auth']], function(){
+    // -------------------------- ROLES --------------------------
     Route::resource('roles', RolController::class);
     Route::resource('usuarios', UsuarioController::class);
     Route::resource('carreras', CarreraController::class);
-
-
-    // Route::get('/carrera', [CarreraController::class, 'verCarreras'])->name('carrera');
     
-    // Route::post('/carrera', [CarreraController::class, 'guardarCarrera'])->name('carrera');
+    Route::post('/agregarUsuario/{id}', [App\Http\Controllers\CarreraController::class, 'agregar_usuario'])->name('agregarUser');
+    Route::delete('/eliminarAtributo/{id}', [App\Http\Controllers\CarreraController::class, 'eliminarAtributo'])->name('eliminarAtributo');
+    Route::delete('/eliminarObjetivo/{id}', [App\Http\Controllers\CarreraController::class, 'eliminarObjetivo'])->name('eliminarObjetivo');
     
-    // Route::patch('/carrera/{id}', [CarreraController::class, 'editarCarrera'])->name('editarCarrera');
-    
-    // Route::delete('/carrera/{id}', [CarreraController::class, 'eliminarCarrera'])->name('eliminarCarrera');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// Auth::routes();
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
