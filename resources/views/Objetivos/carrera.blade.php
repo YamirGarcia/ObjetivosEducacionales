e28d7fd07591209baf06438a870504fb3c45d1b0
Alejandro M <fmelchor9@gmail.com>
2022-04-02 20:00:21 -0600

    Cambios no necesarios

--- resources/views/Objetivos/carrera.blade.php
+++ resources/views/Objetivos/carrera.blade.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="card shadow p-3 mb-5 bg-body rounded">
-                    @if ($carreras->count()==0 and $carrerasAsignadas->count()==0)
+                    @if ($carreras->count()==0)
                         <h1 class="text-center">No existen carreras que mostrar</h1> 
                     @else
                             
@@ -71,45 +71,7 @@
                                 </tr>
                                 @endforeach
 {{-- ------------------------------------------------------------------------------------------------------------------------------------------------------ --}}
-                            {{-- @foreach($carrerasAsignadas as $carrera)
-                                <tr>
-                                    <td>{{$carrera->carrera}}</td>
-                                    <td>{{$carrera->planEstudios}}</td>
-                                    <td>
-                                    
-                                        <button class="badge bg-info text-dark mr-2" data-toggle="modal" data-target="#modalCarreraUsuario{{$carrera->id}}">
-                                            Usuarios: {{$carrera->usuarios->count()}}
-                                        </button>
-                                        <button class="badge bg-success text-dark mr-2" data-toggle="modal" data-target="#modalAtributosCarrera{{$carrera->id}}">
-                                            Atributos: {{$carrera->atributos->count()}}
-                                        </button>
-                                        <button class="badge bg-primary text-dark mr-2" data-toggle="modal" data-target="#modalObjetivosCarrera{{$carrera->id}}">
-                                            Objetivos: {{$carrera->objetivos->count()}}
-                                        </button>
-                                    </td>
-                                    <td>
-                                    <div class="dropdown">
-                                        <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
-                                            Opciones
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            @can('borrar-carrera')
-                                            {!! Form::open(['method' => 'DELETE', 'route' => ['carreras.destroy', $carrera->id],'style'=>'margin: 4px; color:red;']) !!}
-                                            {!! Form::submit('Borrar', ['class' => 'dropdown-item']) !!}
-                                            {!! Form::close() !!}
-                                            @endcan
-                                            @can('editar-carrera')
-                                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalEditar{{$carrera->id}}" style="font-size: 1rem; padding-left: 1.8rem">Editar</a>
-                                            @endcan
-                                            {!! Form::open(['method' => 'GET', 'route' => ['ObjetivoEducacional.show', $carrera->id],'style'=>'margin: 4px']) !!}
-                                            {!! Form::submit('Objetivos Educacionales', ['class' => 'dropdown-item']) !!}
-                                            {!! Form::close() !!}
-                                        </div>
-                                        </div>
-                                        <div class="submenu"></div>                                        
-                                    </td>
-                                </tr>
-                            @endforeach --}}
+                            
                             </tbody>
                         </table>
                         <div class="pagination justify-content-end">
