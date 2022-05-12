b2fa4c8258756bb1510b1447bce2283e72b0ec55
YamirGarcia <84684493+YamirGarcia@users.noreply.github.com>
2022-05-11 20:59:57 -0500

    Y. graficas

--- app/Http/Livewire/Estadisticas/EstadisticasComponent.php
+++ app/Http/Livewire/Estadisticas/EstadisticasComponent.php
@@ -11,12 +11,18 @@ class EstadisticasComponent extends Component
         // dd('here');
 
         $puntos = [];
+        $nombreCarreras = [];
+        $dataBarras3D = [];
         $carreras = \App\Models\Carrera::select('id','carrera')->get();
         foreach($carreras as $carrera){
-            $puntos[] = ['name' => $carrera['carrera'], 'carreras'=>$carrera['carrera'] , 'y' => $carrera['id']];
+            $puntos[] = [$carrera['carrera'] , floatval($carrera['id'])];
+            $nombreCarreras[] = [$carrera['carrera']];
+            $dataBarras3D[] = [$carrera['id']];
         }
         return view('livewire.estadisticas.estadisticas-component', [
             'data' => json_encode($puntos),
+            'carreras' => json_encode($nombreCarreras),
+            'dataBarras3D' => json_encode($dataBarras3D),
         ])->layout('estadisticas.baseEstadisticas');
     }
 }
