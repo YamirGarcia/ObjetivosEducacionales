3a42fe9dc6aae586aacfdb911bd1269cc968b9fa
OverThink3r <sniperfenix20@gmail.com>
2022-05-26 16:46:45 -0500

    Prototipo de dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,11 +18,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="">
-                        <div class="card-body" >
-                            <div class="container-button" style="width: 500px;">
-                                
-                            </div>
-                            <div id="carrusel" class="carousel slide" data-ride="carousel" style="width: 420px;">
+                        <div class="card-body">
+                            
+                            <div id="carrusel" class="carousel slide" data-ride="carousel" style="width: 620px;">
 
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
@@ -202,9 +200,51 @@
                                 </button> --}}
 
                                 <button class="btn-metas" type="button" data-target="#carrusel"
-                                data-slide="prev">Atras</button>
+                                    data-slide="prev">Atras</button>
                                 <button class="btn-metas" type="button" data-target="#carrusel"
-                                data-slide="next">Siguiente</button>
+                                    data-slide="next">Siguiente</button>
+                            </div>
+
+                            <div class="container-button" style="width: 500px;">
+
+                                <div class="card-home ">
+                                    <div class="card-img ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="35" height="35" style="position: relative; right: -60px;"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M319.9 320c57.41 0 103.1-46.56 103.1-104c0-57.44-46.54-104-103.1-104c-57.41 0-103.1 46.56-103.1 104C215.9 273.4 262.5 320 319.9 320zM369.9 352H270.1C191.6 352 128 411.7 128 485.3C128 500.1 140.7 512 156.4 512h327.2C499.3 512 512 500.1 512 485.3C512 411.7 448.4 352 369.9 352zM512 160c44.18 0 80-35.82 80-80S556.2 0 512 0c-44.18 0-80 35.82-80 80S467.8 160 512 160zM183.9 216c0-5.449 .9824-10.63 1.609-15.91C174.6 194.1 162.6 192 149.9 192H88.08C39.44 192 0 233.8 0 285.3C0 295.6 7.887 304 17.62 304h199.5C196.7 280.2 183.9 249.7 183.9 216zM128 160c44.18 0 80-35.82 80-80S172.2 0 128 0C83.82 0 48 35.82 48 80S83.82 160 128 160zM551.9 192h-61.84c-12.8 0-24.88 3.037-35.86 8.24C454.8 205.5 455.8 210.6 455.8 216c0 33.71-12.78 64.21-33.16 88h199.7C632.1 304 640 295.6 640 285.3C640 233.8 600.6 192 551.9 192z"/></svg>
+                                        <h4 class="text-center" style="font-size: 65px">{{count(App\Models\User::all())}}</h4>
+                                        
+                                    </div>
+                                    <div class="card-info">
+                                        <div class="card-text">
+                                            <p class="text-title">Usuarios </p>
+                                            <p class="text-subtitle">Ver más</p>
+                                        </div>
+                                        <div class="card-icon">
+                                            <svg viewBox="0 0 28 25">
+                                                <path
+                                                    d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card-home" >
+                                    <div class="card-img" style="background-color: steelblue "></div>
+                                    <div class="card-info">
+                                        <div class="card-text">
+                                            <p class="text-title">This is a title</p>
+                                            <p class="text-subtitle">This is a subtitle</p>
+                                        </div>
+                                        <div class="card-icon">
+                                            <svg viewBox="0 0 28 25">
+                                                <path
+                                                    d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
